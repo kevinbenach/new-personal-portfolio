23d4393414462f435891bd2d461a512e2e32b028
kevinbenach <kevinbenach@gmail.com>
2025-10-28 10:37:33 +0100

    Fixed linter and updated dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,38 @@
 import js from '@eslint/js';
 import typescript from '@typescript-eslint/eslint-plugin';
-import astro from 'eslint-plugin-astro';
+import tsParser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
-  ...astro.configs.recommended,
   {
-    files: ['**/*.{js,ts,astro}'],
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
     plugins: {
       '@typescript-eslint': typescript,
     },
     rules: {
       'no-unused-vars': 'warn',
       'prefer-const': 'error',
-      'astro/no-unused-css-selector': 'warn',
+    },
+  },
+  {
+    files: ['**/*.test.{js,ts}', '**/test/**/*.{js,ts}'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
     },
   },
 ];
